cfc113f0d9a61b7f0f3e9b71782b95485df90e45
Nightdavisao <nightdavisao27@protonmail.com>
2022-11-10 15:59:39 -0300

    First commit

---
+++ app/src/main/java/io/github/nightdavisao/ihdtxposed/XposedModule.kt
@@ -0,0 +1,29 @@
+package io.github.nightdavisao.ihdtxposed
+
+import android.os.Build
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+
+class XposedModule: IXposedHookLoadPackage {
+    override fun handleLoadPackage(param: XC_LoadPackage.LoadPackageParam?) {
+        if (param != null && param.packageName != "android") return
+
+        val classLoader = param!!.classLoader
+        // com.android.server.power.batterysaver.BatterySaverPolicy
+        val policyClazz = XposedHelpers.findClass("com.android.server.power.batterysaver.BatterySaverPolicy\$Policy", classLoader)
+
+        XposedBridge.hookAllConstructors(policyClazz, object : XC_MethodHook() {
+            override fun afterHookedMethod(paramHook: MethodHookParam?) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    if (paramHook != null) {
+                        XposedHelpers.setBooleanField(paramHook.thisObject, "enableNightMode", false)
+                    }
+                }
+            }
+        })
+    }
+}
